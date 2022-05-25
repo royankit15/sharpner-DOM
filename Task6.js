ba66d660adf3681917ec1f6ed5afcf6771032c21
Ankit Roy <90422666+royankit15@users.noreply.github.com>
2022-05-25 21:06:37 +0530

    DOM/Task6/JS

---
+++ Task6.js
@@ -0,0 +1,12 @@
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor ='green';
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.backgroundColor ='yellow';
+thirdItem.style.color='yellow';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0; i<odd.length;i++)
+    {
+        odd[i].style.backgroundColor ='green';
+    }
