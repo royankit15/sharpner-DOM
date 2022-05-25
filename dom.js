06d61baf4bcdf66f9d4b09241bcfa2fa3f6fcb04
Ankit Roy <90422666+royankit15@users.noreply.github.com>
2022-05-25 13:03:45 +0530

    DOM/Task3/javascript

---
+++ dom.js
@@ -0,0 +1,10 @@
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.forms);
+console.log(document.links);
+console.log(document.images);
